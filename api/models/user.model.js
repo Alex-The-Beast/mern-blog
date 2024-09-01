10d5e337b404c29c40a281a9da88d2dbc991e5e0
Gaurav Kumar <gk064577@gmail.com>
2024-09-02 00:29:19 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose =require('mongoose')
+const validator=require('validator')
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        unique:true,
+        required:[true,"PLease provide your name"]
+
+    },
+    email:{
+        type:String,
+        required:[true,"Email must be provided"],
+        validate:[validator.isEmail,"Provide a valid email"],
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:[true,"Please write a password"],
+        minlength:8
+    },
+
+},{timestamps:true}
+
+)
+
+const User=mongoose.model('User',userSchema)
+
+module.exports=User
