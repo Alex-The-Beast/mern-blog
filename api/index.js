1239779fda7d6ecdf3aad05d4b195e8f8bb63a4e
Gaurav Kumar <gk064577@gmail.com>
2024-09-09 01:51:58 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose =require('mongoose')
 const dotenv =require('dotenv')
 const userRoutes=require('./routes/user.route')
+const authRoutes=require('./routes/auth.route')
 dotenv.config()
 
 mongoose
@@ -15,9 +16,11 @@ mongoose
     console.log(err)
 })
 const app =express()
+app.use(express.json())
 app.listen(3000,()=>{
     console.log('server is running on 3000!')
 })
 
 app.use('/api/v1/user',userRoutes) 
+app.use('/api/v1/auth',authRoutes)
 
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose')
 const validator=require('validator')
 
 const userSchema=new mongoose.Schema({
-    name:{
+     username:{
         type:String,
         unique:true,
         required:[true,"PLease provide your name"]
