0e12830fbd6f5257aa1756b45f093bec1d09c68a
Gaurav Kumar <gk064577@gmail.com>
2024-09-09 03:12:31 +0530

    create sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,8 +15,13 @@ const Header = () => {
     
     <Navbar className='border-b-2'>
         {/* //company logo */}
-        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-600 rounded-lg text-white'>Arnav's </span>Blog</Link>
+        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl
+         font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+            via-purple-500 to-pink-600 rounded-lg text-white'>
+               Arnav's </span>
+                Blog
+            </Link>
 
         {/* //this is for search */}
         <form>
