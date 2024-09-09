cd9af77e1de77594b33d2b8f14ff138bc8f16f60
Gaurav Kumar <gk064577@gmail.com>
2024-09-09 14:28:10 +0530

    add functionality to sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,50 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import {Link,useNavigate} from 'react-router-dom'
+
+
 
 const SignUp = () => {
+  const [formData,setFormData]=useState({})
+  const [errorMessage,setErrorMessage]=useState(null)
+  const [loading,setLoading]=useState(false)
+  const navigate=useNavigate()
+  //storing user data in local host
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()}) //yha spread operator hr form k data ko store rkhta hai and target id btata hai data ka source
+  }
+  //this is for submit data to backend
+  const handleSubmit =async(e)=>{
+    e.preventDefault()
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please Fill Out All Fields')
+    }
+    try{
+      setLoading(true)
+      setErrorMessage(null)
+      const res=await fetch('/api/v1/auth/signup',{
+        method:'POST',   //we add proxy in vit.config bcoz there are port/url diff btw front and backend
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formData)
+      })
+      const data=await res.json();
+      if(data.success === false){
+        return setErrorMessage(data.message)
+       
+      }
+      setLoading(false)
+      // this navigate comes from react router dom ,if everything is ok then it navigate to sign in page
+      if (res.ok){
+          navigate('/sign-in')
+      }
+
+    }catch(error){
+      setErrorMessage(error.message)
+      setLoading(false)
+
+    }
+
+  }
   return (
     <div className='min-h-screen mt-20 '>
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row 
@@ -25,22 +67,35 @@ const SignUp = () => {
         </div>
         {/* right side */}
         <div className="flex-1">
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4'  onSubmit=
+            {handleSubmit}>
             <div>
               <Label value='Your username' ></Label>
-              <TextInput type="text" placeholder="Username" id='username' />
+              <TextInput type="text" placeholder="Username" id='username' onChange=
+              {handleChange}/>
             </div>
 
             <div>
               <Label value='Your Email' ></Label>
-              <TextInput type="text" placeholder="name@company.com" id='email' />
+              <TextInput type="email" placeholder="Email" id='email' onChange=
+              {handleChange}/>
             </div>
 
             <div>
               <Label value='Your password' ></Label>
-              <TextInput type="text" placeholder="Password" id='password' />
+              <TextInput type="password" placeholder="Password" id='password' onChange=
+                {handleChange}/>
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>Sign Up</Button>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>{
+              loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className='pl-3'>Loading....</span>
+                </>
+                
+              ):('Sign Up')
+                   
+             }</Button>
 
           </form>
           <div className='flex gap-2 text-sm mt-5'>
@@ -51,6 +106,12 @@ const SignUp = () => {
                 Sign In
               </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>{errorMessage}</Alert>
+            )
+          }
+          
         </div>
       </div>
     </div>
