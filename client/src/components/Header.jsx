be984b87b4171a1f505e586bcaa99c1f1c0b107e
Gaurav Kumar <gk064577@gmail.com>
2024-09-13 23:23:48 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,16 +3,20 @@ import { Avatar, Dropdown, Navbar } from 'flowbite-react'
 import { Link ,useLocation } from 'react-router-dom'
 import { TextInput } from 'flowbite-react'
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
+import {FaMoon,FaSun} from 'react-icons/fa'
 import { Button } from 'flowbite-react'
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+import {toggleTheme} from '../redux/theme/themeSlice'
 
 
 
 
 const Header = () => {
+   
     const path =useLocation().pathname
+    const dispatch=useDispatch()
     const { currentUser} =useSelector(state =>state.user)
+    const {theme} =useSelector((state)=>state.theme)
   return (
     
     <Navbar className='border-b-2'>
@@ -43,8 +47,10 @@ const Header = () => {
 
          {/* // this is toggle button */}
         <div  className='flex gap-2 md:order-2'>
-            <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-                <FaMoon/>
+            <Button className="w-12 h-10 hidden sm:inline" color="gray" pill
+             onClick={()=>dispatch(toggleTheme())}>
+                {theme === 'light' ? <FaSun/>:<FaMoon/> }
+              
             </Button>
 
 
@@ -69,7 +75,7 @@ const Header = () => {
                 <Dropdown.Item>Profile</Dropdown.Item></Link>
                 <Dropdown.Divider/>
                 <Dropdown.Item>Sign out</Dropdown.Item>
-                
+
 
             </Dropdown>
         ):(
